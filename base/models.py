6d30adbcaf6c95ae5b72c154324cd22781ccb644
MartaFradique <martafradique@ua.pt>
2023-02-01 02:04:56 +0000

    added the deafult databases

--- base/models.py
+++ base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Room(models.Model):
+    # host = models.CharField(max_length=255)
+    # topic
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) # null=True means if the user doesn't enter a description, it will not exist in the database
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
--- base/views.py
+++ base/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 # Create your views here.
+roms = [
+    {
+        'id': 1,
+        'name': 'I cant solve this python problem',
+    },
+     {
+        'id': 2,
+        'name': 'I am having this problem with django',
+    },
+     {
+        'id': 3,
+        'name': 'My react list isnt rendering',
+    },
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'roms': roms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for r in roms:
+        if r['id'] == int(pk):
+            room = r
+            break
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
